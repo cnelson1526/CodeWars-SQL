d09758ff9c190af7c2f2a9306d806470bc665228
Charles Nelson <80921577+cnelson1526@users.noreply.github.com>
2022-10-13 14:29:36 -0700

    Update Remove String Spaces.sql

--- 8kyu/Remove String Spaces.sql
+++ 8kyu/Remove String Spaces.sql
@@ -1,11 +1,12 @@
-Task
+-- Task
 
+/*
 You are given a table
 'nospace' with column 'x', return a table with column 'x' 
 and your result in a column named 'res'.
+*/
 
-
-Solution
+-- Solution
 
 select x, replace(x,' ','') as res
 from nospace;
